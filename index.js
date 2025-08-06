f5ba7d4af76a2ba53990bf0eaa1064f67bf99c50
bigbrainbrian7 <brianhyu08@gmail.com>
2025-08-06 15:51:52 -0700

    mongodb implemented

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -30,21 +32,26 @@ app.get('/', (request, response)=>{
 })
 
 app.get('/api/notes', (request, response)=>{
-    response.json(notes)
+    Note.find({}).then(notes=>{
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (request,response)=>{
-    console.log('bruh')
-    const id = request.params.id
-    console.log(id)
-    const note = notes.find((note)=>note.id===id)
-    console.log(note)
-    if(note) {
-        response.json(note)
-    }
-    else{
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request,response, next)=>{
+    const note = Note.findById(request.params.id)
+    .then(note=>{
+        if(note){
+            response.json(note)
+        }
+        else{
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        // console.log(error)
+        // response.status(400).send({error: "malformed id"})
+        next(error)
+    })
 })
 
 app.post('/api/notes', (request, response)=>{
@@ -56,52 +63,61 @@ app.post('/api/notes', (request, response)=>{
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false
-    }
+    })
 
     const maxID = notes.length > 0 
         ? Math.max(...notes.map((note)=>Number(note.id))) 
         : 0
     note.id=maxID+1
 
-    notes = notes.concat(note)
-    console.log(note)
-    response.json(note)
-    console.log(notes.find((note)=>note.id===maxID+1))
+    note.save().then(saved=>{
+        response.json(saved)
+    })
 })
 
 app.put('/api/notes/:id', (request,response)=>{
     const body = request.body
 
-    if(!body.content){
-        response.status(400).json({
-            error: "content missing"
+    Note.findById(request.params.id)
+    .then(note=>{
+        if (!note){
+            return response.status(404).end()
+        }
+
+        note.content=body.content
+        note.important=body.important
+
+        return note.save().then(updatedNote=>{
+            response.json(updatedNote)
         })
-    }
+    })
+    .catch(error=>next(error))
 
-    const note = {
-        content: body.content,
-        important: body.important,
-        id: body.id
-    }
-
-    notes = notes.map((bruh)=>bruh.id===note.id ? note : bruh)
-
-    response.json(note)
 })
 
-app.delete('/api/notes/:id', (request, response)=>{
-    const id = request.params.id
-    notes = notes.filter((note)=> note.id!==id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next)=>{
+    Note.findByIdAndDelete(request.params.id)
+    .then(result=>{
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+const errorHandler = (error, request, response,next)=>{
+    console.log(error.message)
 
+    if(error.name=== 'CastError') {
+        return response.status(400).send({error: 'malformed id'})
+    }
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+    next(error)
+}
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`)
 })
